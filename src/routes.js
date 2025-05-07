13302151cb7c538db9bbc4dfdef3d2ccebd3e445
Andre Alves Miguel <andre_alvesmiguel@hotmail.com>
2025-05-07 17:34:07 -0300

    esqueci de adicionar as rotas >_<,agora ta certo

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import adm from './Controller/admController.js';
 import cliente from './Controller/clienteController.js';
 import pedidos from './Controller/pedidosController.js';
+import produto from './Controller/produtoController.js';
+import variantes from './Controller/variantesController.js';
 
 export default function adcionarRotas(server){
     server.use(adm);
     server.use(cliente);
     server.use(pedidos);
+    server.use(produto);
+    server.use(variantes);
 }
